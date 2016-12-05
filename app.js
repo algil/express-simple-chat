46aeb296c877df2b9fe10762dfe7fd0432cca3d9
algil <antonio.luis.gil@gmail.com>
2016-12-05 10:39:26 +0100

    First version of the chat app

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+const publicDir = express.static(`${__dirname}/public`);
+
+app.use(publicDir);
+
+app.get('/', (req, res) => res.sendFile(`${publicDir}/index.html`));
+
+http.listen(port, () => console.log(`Running server on localhost:${port}`));
+
+io.on('connection', (socket) => {
+    socket.broadcast.emit('new user', {message: 'New user has been connected'});
+
+    socket.on('new message', message => socket.emit('new message', message));
+
+    socket.on('disconnect', () => socket.broadcast.emit('user disconnected', {message: 'An user left the chat'}));
+});
---
+++ public/chat.js
@@ -0,0 +1,27 @@
+(function(io) {
+    'use strict';
+
+    var io = io();
+    var chatForm = document.querySelector('#chat-form');
+    var messageText = document.querySelector('#message-text');
+    var chat = document.querySelector('#chat');
+
+    chatForm.onsubmit = function(event) {
+        event.preventDefault();
+        io.emit('new message', messageText.value);
+        messageText.value = null;
+        return false;
+    }
+
+    io.on('new user', function(newUser) {
+        alert(newUser.message);
+    });
+
+    io.on('user disconnected', function(data) {
+        alert(data.message);
+    });
+
+    io.on('new message', function (message) {
+        chat.insertAdjacentHTML('beforeend', '<li>' + message + '</li>');
+    });
+})(io);
